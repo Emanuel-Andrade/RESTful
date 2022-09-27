d336a8665f7701d4f8cc33e2d2c5903694e0248c
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-09-27 18:20:46 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 0,
+    'prettier/prettier': 'error'
+  },
+};
