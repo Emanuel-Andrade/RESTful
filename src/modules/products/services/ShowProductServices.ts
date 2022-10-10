88576980b6a6fa057acb16ddd6a8e3949d508883
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-10-10 20:10:09 -0300

    adding show product service

---
+++ src/modules/products/services/ShowProductServices.ts
@@ -0,0 +1,13 @@
+import AppError from 'src/shared/errors/appError';
+import Product from '../typeorm/entities/Product';
+import ProductRepository from '../typeorm/ProductsRepository/ProductsRepository';
+
+class CreateProduct {
+  public async show(id: string): Promise<Product> {
+    const product = await ProductRepository.findOne(id);
+
+    if (!product) throw new AppError('There is no product with this id');
+    return product;
+  }
+}
+export default new CreateProduct();
