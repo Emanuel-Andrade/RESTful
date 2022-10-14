916dfe02be3b3b3e9bb87b4d42ffc70a7eeae445
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-10-14 19:25:12 -0300

    adding User controller creat and list method

---
+++ src/modules/users/controllers/UserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUsersService from '../services/ListUsersService';
+
+class UserController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const result = await ListUsersService.list();
+    return res.json(result);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const data = req.body;
+    const result = await CreateUserService.create(data);
+    return res.json(result);
+  }
+}
+
+export default new UserController();
