9fa094c0d9de7ba6ad4de6cbe4be74df8ccda4f5
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-10-31 19:40:51 -0300

    corregting methods name

--- src/modules/customers/services/DeleteCustomerProfile.ts
+++ src/modules/customers/services/DeleteCustomerProfile.ts
@@ -3,9 +3,9 @@ import AppError from '../../../shared/errors/appError';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
 class ShowCustomerProfile {
-  public async findById(user_id: string): Promise<void> {
+  public async delete(customer_id: string): Promise<void> {
     const customCustomerRepository = getCustomRepository(CustomersRepository);
-    const customer = await customCustomerRepository.findById(user_id);
+    const customer = await customCustomerRepository.findById(customer_id);
     if (customer === undefined) throw new AppError('There is no user');
 
     await customCustomerRepository.remove(customer);
