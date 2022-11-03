96bc22552ffab4530db65965740a1b69733e98ba
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-11-03 20:17:25 -0300

    adding find by Id in array method

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,5 +1,10 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Product';
+
+interface IFindProducts {
+  id_product: string;
+  quantity: number;
+}
 
 @EntityRepository(Product)
 class ProductRepository extends Repository<Product> {
@@ -11,6 +16,17 @@ class ProductRepository extends Repository<Product> {
     });
 
     return product;
+  }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const ids = products.map((product) => product.id_product);
+
+    const findProducts = await this.find({
+      where: {
+        id: In(ids),
+      },
+    });
+    return findProducts;
   }
 }
 
