970da5401dae10a7b507661491641ab0d95c2933
Emanuel-Andrade <emanuel.mtg.96@gmail.com>
2022-11-07 19:56:54 -0300

    correcting order routes name

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,7 +5,7 @@ import LoginRoutes from 'src/modules/users/routes/login.routes';
 import passwordRoutes from 'src/modules/users/routes/password.routes';
 import ProfileRoutes from 'src/modules/users/routes/profile.routes';
 import CustomerRoutes from 'src/modules/customers/routes/customers.routes';
-import OrderRoutes from 'src/modules/orders/routes/product.routes';
+import OrderRoutes from 'src/modules/orders/routes/orders.routes';
 
 const routes = Router();
 
